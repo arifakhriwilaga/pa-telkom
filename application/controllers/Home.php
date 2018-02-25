04927775d8eff2241fd28434a5b07ed65a9ebceb
ari_fakhri <ari.fakhri@appschef.com>
2018-02-25 16:08:18 +0700

    add function show notification;

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -2,62 +2,65 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Home extends CI_Controller {
+
 	public function __construct() {
-        parent::__construct();
-    }
+    parent::__construct();
+		// $base = $this->load->library('Base');
+  }
 
 	public function index()	{
 		$page_title = "Home";
+
 		$data = array(
 			'page_title' => $page_title
 		);
 
-		$this->load->render('FE_PA/index');
+		$this->load->render('front_end/index',$data);
 	}
 	
 	public function healthy_info()	{
-		$page_title = "Info Kesehatan";
+		$page_title = "Info Sehat";
 		$data = array(
 			'page_title' => $page_title
 		);
 
-		$this->load->render('FE_PA/healthy_info');
-	}
-
-	public function visit() {
-		$page_title = "Visit";
-		$data = array(
-			'page_title' => $page_title
-		);
-
-		$this->load->render('FE_PA/visit');
+		$this->load->render('front_end/guest/healthy_info/healthy_info',$data);
 	}
 
 	public function check_step_1() {
-		$page_title = "Periksa | Step 1";
+		$page_title = "Periksa";
 		$data = array(
 			'page_title' => $page_title
 		);
 
-		$this->load->render('FE_PA/step_1_form_check');
+		$this->load->render('front_end/guest/check/step_1_form_check',$data);
 	}
 
 	public function check_step_2() {
-		$page_title = "Periksa | Step 2";
+		$page_title = "Periksa";
 		$data = array(
 			'page_title' => $page_title
 		);
 
-		$this->load->render('FE_PA/step_2_form_check');
+		$this->load->render('front_end/guest/check/step_2_form_check',$data);
 	}
 
 	public function check_step_final() {
-		$page_title = "Periksa | Step Final";
+		$page_title = "Periksa";
 		$data = array(
 			'page_title' => $page_title
 		);
 
-		$this->load->render('FE_PA/step_final_form_check');
+		$this->load->render('front_end/guest/check/step_final_form_check',$data);
+	}
+
+	public function visit() {
+		$page_title = "Kunjungan";
+		$data = array(
+			'page_title' => $page_title
+		);
+
+		$this->load->render('front_end/auth/visit/visit',$data);
 	}
 
 	public function consul_doctor() {
@@ -66,6 +69,15 @@ class Home extends CI_Controller {
 			'page_title' => $page_title
 		);
 
-		$this->load->render('FE_PA/consul_doctor');
+		$this->load->render('front_end/auth/consul_doctor/consul_doctor',$data);
+	}
+
+	public function notification() {
+		$page_title = "Notifikasi";
+		$data = array(
+			'page_title' => $page_title
+		);
+
+		$this->load->render('front_end/auth/user/notification',$data);
 	}
 }
