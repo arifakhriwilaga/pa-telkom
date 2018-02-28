e484cb51d0a2c2db35a94fc1e1b5fa0c54623787
ari_fakhri <ari.fakhri@appschef.com>
2018-02-28 16:32:06 +0700

    make controller and layout edit-profile

---
+++ application/controllers/front_end/Profile.php
@@ -0,0 +1,22 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Profile extends CI_Controller {
+
+	public function __construct() {
+    parent::__construct();
+    $user = $this->session->userdata('user');
+    if ($user['level_user'] == 'admin') {
+    	redirect('dasbor');
+    }
+  }
+
+	public function index()	{
+		$page_title = "Edit Profile";
+		$data = array(
+			'page_title' => $page_title
+		);
+
+		$this->load->render('front_end/user/edit_profile',$data);
+	}
+}
