3b077cbc9aeff46e34d28a757549036cf3324d53
ari_fakhri <ari.fakhri@appschef.com>
2018-03-01 08:51:56 +0700

    make layout, route and link to detail-notification

--- application/config/routes.php
+++ application/config/routes.php
@@ -90,6 +90,7 @@ $route['periksa/step-2'] = 'front_end/check/check_step_2';
 $route['periksa/step-final'] = 'front_end/check/check_step_final';
 $route['konsul-dokter'] = 'front_end/consul_doctor';
 $route['notifikasi'] = 'front_end/notification';
+$route['detail-notifikasi'] = 'front_end/notification/detail';
 
 // Routes CMS
 $route['dasbor'] = 'cms/dashboard';
