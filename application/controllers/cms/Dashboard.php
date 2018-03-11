268b68ff7c8e4fe8798241b2338972d0205ac18f
ari_fakhri <ari.fakhri@appschef.com>
2018-03-11 13:06:46 +0700

    design new layout dashboard

--- application/controllers/cms/Dashboard.php
+++ application/controllers/cms/Dashboard.php
@@ -2,10 +2,15 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Dashboard extends CI_Controller {
+	private $born_date;
 
 	public function __construct() {
     parent::__construct();
     $user = $this->session->userdata('user');
+    $date = preg_split('/\-/', strval($user['born_date']));
+    
+    $this->born_date = $date[2] . '/' . $date[1] . '/' . $date[0];
+    // var_dump($born_date);exit();
     if (empty($user) || $user['level_user'] == 'user') {
     	redirect('/');
     }
@@ -15,7 +20,8 @@ class Dashboard extends CI_Controller {
 		$page_title = "Dasbor";
 
 		$data = array(
-			'page_title' => $page_title
+			'page_title' => $page_title,
+			'born_date' => $this->born_date
 		);
 
 		$this->load->render('cms/dashboard/dashboard', $data);
